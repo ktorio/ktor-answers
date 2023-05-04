c6efd0ca1a5061c9ba5ffffd5d53d9bf90bd7a32
asm0dey <pavel.finkelshtein@gmail.com>
2023-05-04 16:44:03 +0200

    Adds liquibase migration to the application start

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 group = "io.ktor.answers"
 version = "0.0.1"
 application {
-    mainClass.set("io.ktor.answers.ApplicationKt")
+    mainClass.set("io.ktor.server.netty.EngineMain")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
