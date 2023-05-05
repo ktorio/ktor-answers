c0595ea9b17a75558018c9eab04146344ea4a146
asm0dey <pavel.finkelshtein@gmail.com>
2023-05-05 19:11:23 +0200

    Migrates to gradle version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,7 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val exposedVersion: String by project
-
 plugins {
-    kotlin("jvm") version "1.8.21"
-    id("io.ktor.plugin") version "2.3.0"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.21"
+    alias(libs.plugins.org.jetbrains.kotlin.jvm)
+    alias(libs.plugins.io.ktor.plugin)
+    alias(libs.plugins.org.jetbrains.kotlin.plugin.serialization)
 }
 
 group = "io.ktor.answers"
@@ -23,23 +18,23 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-host-common-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-status-pages-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
-    implementation("org.liquibase:liquibase-core:4.21.1")
-    runtimeOnly("org.postgresql:postgresql:42.6.0")
+    implementation(libs.ktor.server.core.jvm)
+    implementation(libs.ktor.server.content.negotiation.jvm)
+    implementation(libs.ktor.serialization.kotlinx.json.jvm)
+    implementation(libs.ktor.server.host.common.jvm)
+    implementation(libs.ktor.server.status.pages.jvm)
+    implementation(libs.ktor.server.netty.jvm)
+    implementation(libs.logback.classic)
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.dao)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.exposed.kotlin.datetime)
+    implementation(libs.liquibase)
+    runtimeOnly(libs.postgres)
     testImplementation(kotlin("test"))
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
-    testImplementation("org.testcontainers:postgresql:1.18.0")
-    testImplementation("org.testcontainers:junit-jupiter:1.18.0")
+    testImplementation(libs.ktor.server.tests.jvm)
+    testImplementation(libs.testcontainers.postgres)
+    testImplementation(libs.testcontainers.jupiter)
 }
 
 tasks.test {
