bcd96d0c41ff342352a432d9abfc03d787e7b10d
asm0dey <pavel.finkelshtein@gmail.com>
2023-05-10 16:00:08 +0200

    Fixes tests for Mac M1

--- src/test/kotlin/io/ktor/answers/AbstractDbTest.kt
+++ src/test/kotlin/io/ktor/answers/AbstractDbTest.kt
@@ -2,11 +2,15 @@ package io.ktor.answers
 
 import org.jetbrains.exposed.sql.Database
 import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy
+import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy
+import java.time.Duration
 
 abstract class AbstractDbTest {
     companion object {
         @JvmField
-        var postgres = PostgreSQLContainer("postgres")
+        val postgres = PostgreSQLContainer("postgres")
+            .waitingFor(HostPortWaitStrategy())
 
         init {
             postgres.start()
