c5835dabc2b9c694215b1bbe17528325761aa3ac
Garth Gilmour <garth.gilmour@jetbrains.com>
2023-06-01 15:16:51 +0100

    Use nested locations on 'questions2' endpoint

--- src/main/kotlin/io/ktor/answers/routing/QuestionsRouting.kt
+++ src/main/kotlin/io/ktor/answers/routing/QuestionsRouting.kt
@@ -5,21 +5,25 @@ import io.ktor.answers.fakedb.model.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.locations.*
+import io.ktor.server.locations.post
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 @OptIn(KtorExperimentalLocationsAPI::class)
-@Location("/questions2/{id}")
-data class QuestionPath(val id: Int)
+@Location("/questions2")
+object QuestionsPath {
+    @Location("/{id}")
+    data class ById(val path: QuestionsPath = QuestionsPath, val id: Int)
+}
 
 @OptIn(KtorExperimentalLocationsAPI::class)
 fun Routing.questionsRouting(questionsRepository: QuestionsRepository) {
-    get("/questions2") {
+    get<QuestionsPath> {
         call.respond(questionsRepository.getQuestions())
     }
 
-    get<QuestionPath> { path ->
+    get<QuestionsPath.ById> { path ->
         val question = questionsRepository.getQuestionById(path.id)
         if(question != null) {
             call.respond(question)
@@ -28,25 +32,7 @@ fun Routing.questionsRouting(questionsRepository: QuestionsRepository) {
         }
     }
 
-    /*
-    get("/questions2/{id}") {
-        val id = call.parameters["id"]
-        if(id == null) {
-            call.respond(HttpStatusCode.BadRequest, "No ID")
-            return@get
-        }
-        try {
-            val question = questionsRepository.getQuestionById(id.toInt())
-            if(question != null) {
-                call.respond(question)
-            } else {
-                call.respond(HttpStatusCode.NotFound)
-            }
-        } catch(e: NumberFormatException) {
-            call.respond(HttpStatusCode.BadRequest, "'$id' is not an integer")
-        }
-    } */
-    post("/questions2") {
+    post<QuestionsPath> {
         val newQuestionData: QuestionData = call.receive()
         val question = questionsRepository.addQuestion(newQuestionData)
         if (question == null) {
@@ -57,7 +43,7 @@ fun Routing.questionsRouting(questionsRepository: QuestionsRepository) {
         }
     }
 
-    delete<QuestionPath> { path ->
+    delete<QuestionsPath.ById> { path ->
         if(questionsRepository.deleteQuestionById(path.id)) {
             call.respond(HttpStatusCode.OK)
         } else {
@@ -65,24 +51,6 @@ fun Routing.questionsRouting(questionsRepository: QuestionsRepository) {
         }
     }
 
-    /*
-    delete("/questions2/{id}") {
-        val id = call.parameters["id"]
-        if(id == null) {
-            call.respond(HttpStatusCode.BadRequest, "No ID")
-            return@delete
-        }
-        try {
-            if(questionsRepository.deleteQuestionById(id.toInt())) {
-                //call.respondText("Question '$id' deleted")
-                call.respond(HttpStatusCode.OK)
-            } else {
-                call.respond(HttpStatusCode.NotFound)
-            }
-        } catch(e: NumberFormatException) {
-            call.respond(HttpStatusCode.BadRequest, "'$id' is not an integer")
-        }
-    }*/
     get("/users2") {
         call.respond(questionsRepository.getUsers())
     }
